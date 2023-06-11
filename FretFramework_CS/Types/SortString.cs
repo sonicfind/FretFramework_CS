7d90d1bedad87f85a0c48cd4454087eb544966d6
sonicfind <sonicfind2@gmail.com>
2023-06-10 21:25:36 -0500

    Introduce SortString type
    Constructs a lowercase version of the given string for use in sorting (will need to be updated later to account for special tags)

---
+++ FretFramework_CS/Types/SortString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Types
+{
+    public struct SortString
+    {
+        private string _str;
+        private string sortStr;
+
+        public string Str
+        {
+            get { return _str; }
+            set {
+                _str = value;
+                sortStr = value;
+            }
+        }
+        
+        public readonly string SortStr { get { return sortStr; } }
+        public SortString()
+        {
+            _str = string.Empty;
+            sortStr = string.Empty;
+        }
+
+        public SortString(string str)
+        {
+            _str = str;
+            sortStr = str.ToLower();
+        }
+    }
+}
