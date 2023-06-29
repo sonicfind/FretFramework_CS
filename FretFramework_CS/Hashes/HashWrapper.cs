38b4883eb1ca869a12e9d3f0f66d943a3db06a0b
sonicfind <sonicfind2@gmail.com>
2023-06-29 17:56:12 -0500

    HashWrapper touchups
    + Add missing hashcode setter from binaryreader constructor
    
    + Implement IComparable<> with additonal proper Equals()

--- FretFramework_CS/Hashes/HashWrapper.cs
+++ FretFramework_CS/Hashes/HashWrapper.cs
@@ -9,11 +9,11 @@ using System.Threading.Tasks;
 
 namespace Framework.Hashes
 {
-    public abstract class HashWrapper
+    public abstract class HashWrapper : IComparable<HashWrapper>
     {
         protected readonly int[] hash;
         private readonly int hashCode;
-        protected HashWrapper(int length) { hash = new int[length]; }
+        private HashWrapper(int length) { hash = new int[length]; }
 
         protected HashWrapper(byte[] hash, int length) : this(length)
         {
@@ -35,7 +35,7 @@ namespace Framework.Hashes
         protected HashWrapper(BinaryReader reader, int length) : this(length)
         {
             for (int i = 0; i < hash.Length; i++)
-                hash[i] = reader.ReadInt32();
+                hashCode ^= hash[i] = reader.ReadInt32();
         }
 
         public void Write(BinaryWriter writer)
@@ -57,12 +57,35 @@ namespace Framework.Hashes
             return bytes;
         }
 
-        public bool Equals(HashWrapper other)
+        public override bool Equals(object? o)
         {
+            if (o is not HashWrapper other)
+                return false;
+
             for (int i = 0; i < hash.Length; ++i)
                 if (other.hash[i] != hash[i])
                     return false;
             return true;
+        }
+
+        public int CompareTo(HashWrapper? other)
+        {
+            for (int i = 0; i < hash.Length; ++i)
+                if (hash[i] < other!.hash[i])
+                    return -1;
+                else if (hash[i] > other.hash[i])
+                    return 1;
+            return 0;
+        }
+
+        public static bool operator==(HashWrapper lhs, HashWrapper rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator!=(HashWrapper lhs, HashWrapper rhs)
+        {
+            return !lhs.Equals(rhs);
         }
 
         public override int GetHashCode()
@@ -73,14 +96,12 @@ namespace Framework.Hashes
 
     public class SHA1Wrapper : HashWrapper
     {
-        public SHA1Wrapper() : base(5) { }
         public SHA1Wrapper(byte[] hash) : base(hash, 5) { }
         public SHA1Wrapper(BinaryReader reader) : base(reader, 5) { }
     }
 
     public class MD5Wrapper : HashWrapper
     {
-        public MD5Wrapper() : base(4) { }
         public MD5Wrapper(byte[] hash) : base(hash, 4) { }
 
         public MD5Wrapper(BinaryReader reader) : base(reader, 4) { }
